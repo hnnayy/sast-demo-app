a0b5581ad7148411d08983a4db43b74b7e5fca65
hnnayy <mutiahanin2017@gmail.com>
2025-05-04 17:29:12 +0700

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-def greet(name): print(f"Hello, {name}!")
-
-def run_command(cmd): subprocess.call(cmd, shell=True)
-if   name   == "  main  ":
-name = input("Enter your name: ") greet(name)
-cmd = input("Enter a command to run: ") run_command(cmd)
